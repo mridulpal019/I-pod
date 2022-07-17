d83e1bb0e15da3a64870538c1ee37be71010d33e
Mridul <mridulpal19@gmail.com>
2022-07-18 04:04:01 +0530

    add navigation controller

--- src/pages/Controller.js
+++ src/pages/Controller.js
@@ -1,15 +1,16 @@
 import React,{useState} from "react";
 import { useNavigate } from "react-router-dom";
 import ZingTouch from 'zingtouch';
-import List from "../config/List";
+
 
 
 const Controller=()=>{
      const navigate=useNavigate();
-    //  const menu=(e)=>{
-    //   e.preventDefault();
-    //   navigate('/')
-    console.log(window.location.pathname,"path name")
+  
+    const page=window.location.pathname;
+    var  f=15;
+    var direction;
+    var navigateTo;
     const List=[
       [
        "cover-flow",
@@ -23,25 +24,34 @@ const Controller=()=>{
       "albums"
       ],   
     ]
-    console.log(List[0]);
+    if (page=="/"){
+      var  activeList=List[0]
+    }else if(page=="/music"){
+      var activeList=List[1];
 
-    //  }
+    }
+
     const home=(e)=>{
       e.preventDefault();
       navigate('/')
 
     }
     var touchArea;
-  
     var angle;
-    // console.log(angle)
-    var direction;
+    const loadto=()=>{
+     console.log(typeof(navigateTo))
+      navigate(navigateTo);
+    }
     const tt=()=>{
     touchArea = document.getElementById('controller');
     const myRegion = new ZingTouch.Region(touchArea);
     console.log('called')
-    var f=15;
-    let direction;
+    f=15;
+    var i=0;
+    
+    const activeLenght=activeList.length;
+    console.log(activeLenght,"active length");
+    
 
     myRegion.bind(touchArea, 'rotate', function(e){
          angle=e.detail.angle;
@@ -50,12 +60,26 @@ const Controller=()=>{
          if( Math.abs(e.detail.distanceFromOrigin) > f){
           console.log("bingo",f);
           f+=15;
+          const active=document.getElementById(activeList[i]);
+          navigateTo="/" +activeList[i];
+          if (i== 0){
+            var j=activeLenght-1;
+            console.log(j,"inside if")
+          }else{
+          var j=Math.abs((i-1))%(activeLenght);
+          }
+          const inactive=document.getElementById(activeList[j]);
+          inactive.className="";
+          // console.log(active,"after")
+          active.className = "active";
+          i=(i+1)%activeLenght;
+          console.log(active);
           if (e.detail.distanceFromLast>0){
             if (!direction){
               direction='clockwise';
             }else if(direction == 'anticlockwise'){
                   direction ='clockwise'
-                  f=15;                 
+                  // f=15;                 
             }        
           
             console.log('Yes')
@@ -64,22 +88,20 @@ const Controller=()=>{
               direction='anticlockwise';
             }else if(direction == 'clockwise'){
                   direction ='anticlockwise'
-                  f=15;   
+                  // f=15;   
               console.log("no")              
             }   
-
            }
-          
          }
-         console.log(e.detail)
+
         
    });
   }
-    
+
     return (
-        <div className='pod-controller' id='controller' onMouseEnter={tt}>
-        <div id='Menu' onClick={home}><h3>Menu</h3></div>
-        <div className='controller-mid' > <i className="fa-solid fa-backward-fast"></i> <div id='enter' className='enter'></div>  <i className="fas fa-forward-fast"></i></div>
+        <div className='pod-controller' id='controller' onMouseEnter={tt} >
+        <div id='Menu' onClick={home}><h3><a>Menu </a></h3></div>
+        <div className='controller-mid' > <i className="fa-solid fa-backward-fast"></i> <div id='enter' className='enter' onClick={loadto}></div>  <i className="fas fa-forward-fast"></i></div>
         <div className='play-pause'><i className="fas fa-play"></i> <i className="fas fa-regular fa-pause"></i></div>
       
       </div>
