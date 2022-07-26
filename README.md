df6118fc4fb644536bfa44a3286f00731338f9bf
Mridul <mridulpal19@gmail.com>
2022-07-26 07:16:51 +0530

    final edit

--- README.md
+++ README.md
@@ -1,70 +1,30 @@
-# Getting Started with Create React App
+##for installing the required modules run :-
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+npm install
 
-## Available Scripts
+#after installing run the project by navigating to the project directory in the terminal and write:-
 
-In the project directory, you can run:
+npm start
 
-### `npm start`
+##features
+#controller
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Menu button is used to navigate to home screen.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+the controller can be used to navigate to different sections
 
-### `npm test`
+controller is made up by zingtouch--rotate  module.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+#In music section:-
 
-### `npm run build`
+sub menu is there in which allsongs section contains songs
+by clicking on centre button(music can be palyed and paused)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+#in albums section
+an image of group of images has been placed.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+#hosted project link:-
+https://i-pod.vercel.app/
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/pages/Controller.js
+++ src/pages/Controller.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import ZingTouch from 'zingtouch';
 import Song from "../assets/songs/life.mp3"
@@ -7,11 +7,21 @@ import Song from "../assets/songs/life.mp3"
 
 const Controller=()=>{
      const navigate=useNavigate();
+     const [navigateTo,setNavigate]=useState(null);
+
+
+
+     // on page load call the funvtion setcontroller to zingtouch object; 
+     useEffect(()=>{
+      console.log("load tt");
+      setcontroller();
+     })
+
     //page will get the current page so that we can set activelist to chnage the options in home/music
     const page=window.location.pathname;
     var  f=15;
-    var direction;
-    var navigateTo;
+ 
+
     const List=[
       [
        "coverflow",
@@ -35,24 +45,25 @@ const Controller=()=>{
     }
 //  on click of menu the display will be redirected to Home page
     const home=(e)=>{
-      e.preventDefault();
-      navigate('/')
+          e.preventDefault();
+          navigate('/')
 
     }
     // defineng variable for zingtouch
     var touchArea;
     var angle;
+
     // audio for palying in allsongs in music
     var audio = new Audio(Song);
     var playing;
     const play=()=>{
-      if(playing === true){
-        audio.pause();
-        playing=false;
-      }else{
-      audio.play();
-      playing=true;
-    } 
+          if(playing === true){
+              audio.pause();
+              playing=false;
+          }else{
+            audio.play();
+            playing=true;
+          } 
     }
 // on click of enter button it will first check if page is all songs if it is then audio will be played/pause
 // else it will navigate to the highlighted section
@@ -61,72 +72,64 @@ const Controller=()=>{
         if(page =="/allsongs"){
           play();
         }else{
-     console.log(navigateTo)
-      navigate(navigateTo);
+    //  console.log(navigateTo)
+          navigate(navigateTo);
+        }
     }
-    }
-    
-    const tt=()=>{
-    touchArea = document.getElementById('controller');
-    const myRegion = new ZingTouch.Region(touchArea);
-    console.log('called')
-    f=15;
-    var i=0;
-    
-    const activeLenght=activeList.length;
-    console.log(activeLenght,"active length");
-    
 
-    myRegion.bind(touchArea, 'rotate', function(e){
-         angle=e.detail.angle;
-         console.log(direction)
-         
-         if( Math.abs(e.detail.distanceFromOrigin) > f){
-          console.log("bingo",f);
-          f+=15;
-          const active=document.getElementById(activeList[i]);
-          navigateTo="/" +activeList[i];
-          if (i== 0){
-            var j=activeLenght-1;
-            console.log(j,"inside if")
-          }else{
-          var j=Math.abs((i-1))%(activeLenght);
-          }
-          const inactive=document.getElementById(activeList[j]);
-          inactive.className="";
-          // console.log(active,"after")
-          active.className = "active";
-          i=(i+1)%activeLenght;
-          console.log(active);
-          if (e.detail.distanceFromLast>0){
-            if (!direction){
-              direction='clockwise';
-            }else if(direction == 'anticlockwise'){
-                  direction ='clockwise'
-                  // f=15;                 
-            }        
+    const setcontroller=()=>{
+      if(page == "/" || page== "/music"){
+          touchArea = document.getElementById('controller');
+          const myRegion = new ZingTouch.Region(touchArea);
+
+          f=15;
+          var i=0;
           
-            console.log('Yes')
-           }else{
-            if (!direction){
-              direction='anticlockwise';
-            }else if(direction == 'clockwise'){
-                  direction ='anticlockwise'
-                  // f=15;   
-              console.log("no")              
-            }   
-           }
-         }
+          const activeLenght=activeList.length;
+          
 
+          myRegion.bind(touchArea, 'rotate', function(e){
+              angle=e.detail.angle;         
+              if( Math.abs(e.detail.distanceFromOrigin) > f){
+                console.log("bingo",f);
+                f+=15;
+                const active=document.getElementById(activeList[i]);
+                
+                if (i== 0){
+                  var j=activeLenght-1;
+                  console.log(j,"inside if")
+                }else{
+                var j=Math.abs((i-1))%(activeLenght);
+                }
+                const inactive=document.getElementById(activeList[j]);
+                // console.log(inactive);
+                inactive.className="";
+                // console.log(active,"after")
+                active.className = "active";
+                setNavigate("/"+activeList[i]);
+                i=(i+1)%activeLenght;
+                // console.log(active);
+              }
         
-   });
-  }
+        
+          });
+      }
+    }
 
     return (
-        <div className='pod-controller' id='controller' onMouseEnter={tt} >
-        <div id='Menu' onClick={home}><h3><a>Menu </a></h3></div>
-        <div className='controller-mid' > <i className="fa-solid fa-backward-fast"></i> <div id='enter' className='enter' onClick={loadto}></div>  <i className="fas fa-forward-fast"></i></div>
-        <div className='play-pause'><i className="fas fa-play"></i> <i className="fas fa-regular fa-pause"></i></div>
+      <div className='pod-controller' id='controller' >
+          <div id='Menu' onClick={home}>
+            <h3><a>Menu </a></h3>
+          </div>
+          <div className='controller-mid' >
+                <i className="fa-solid fa-backward-fast"></i>
+                <div id='enter' className='enter' onClick={loadto}></div> 
+                <i className="fas fa-forward-fast"></i>
+          </div>
+          <div className='play-pause'>
+              <i className="fas fa-play"></i> 
+              <i className="fas fa-regular fa-pause"></i>
+          </div>
       
       </div>
     ) ;
